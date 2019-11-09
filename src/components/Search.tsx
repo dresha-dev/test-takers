26638a89fda88c7a84c52d08cccd2c3c092a6bc8
adreyponamarev <andrey.ponamarev@maxymiser.com>
2019-11-09 16:58:55 +0100

    Add styles to search field

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,8 +1,47 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import * as SearchActions from '../store/actions/searchActions'
+import styled from 'styled-components'
+import { InputBase } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
 
-const Search = (props) => {
+const SearchWrapper = styled.div(
+  ({ theme }) => `
+  position: relative;
+  border-radius: ${theme.shape.borderRadius},
+  background-color: fade(${theme.palette.common.white}, 0.15);
+  
+  :hover {
+    background-color: fade(${theme.palette.common.white}, 0.25),
+  }  
+`,
+)
+
+const SearchIconWrapper = styled.div(
+  ({ theme }) => `
+  width: ${theme.spacing(7)}px;
+  height: 100%;
+  position: absolute;
+  pointer-events: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`,
+)
+
+const SearchInput = styled(InputBase)(
+  ({ theme }) => `
+  color: inherit;
+
+  .MuiInputBase-input{
+    padding: ${theme.spacing(1, 1, 1, 7)};
+    transition: ${theme.transitions.create('width')};
+    width: 100%;
+  }
+`,
+)
+
+const Search: React.FC = () => {
   const dispatch = useDispatch()
   const [value, setValue] = useState('')
 
@@ -10,15 +49,26 @@ const Search = (props) => {
     setValue(event.target.value)
   }
 
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault()
-    dispatch(SearchActions.findUserByName(value))
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      dispatch(SearchActions.findUserByName(value))
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={value} name="user_search" onChange={handleChange} />
-    </form>
+    <SearchWrapper>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <SearchInput
+        value={value}
+        placeholder="Search..."
+        inputProps={{ 'aria-label': 'search' }}
+        onKeyPress={handleKeyPress}
+        onChange={handleChange}
+      />
+    </SearchWrapper>
   )
 }
 
