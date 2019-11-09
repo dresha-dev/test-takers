d47775fcecccdc18efc119a4da191ec14ecc8c63
adreyponamarev <andrey.ponamarev@maxymiser.com>
2019-11-09 11:55:29 +0100

    Init project

---
+++ src/components/User.tsx
@@ -0,0 +1,5 @@
+const User = props => {
+  return <p>Hello</p>
+}
+
+export default User
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import User from '../components/User'
+
+const Home = () => <User />
+
+export default Home
